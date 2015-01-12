4e5449d2d733d0b9b25ec38853f1a0242db6d9b1
Niklas Claesson <nicke.claesson@gmail.com>
2015-01-12 01:14:45 +0100

    Splitting page into multiple components

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -4,8 +4,23 @@
  *
  * @see services
  */
-var controllers = angular.module('ExampleApp.controllers', [])
-    .controller('ExampleController', function ($scope, UserdataService) {
+var controllers = angular.module('SmawgApp.controllers', [])
+    .controller('HomeController', function ($scope, UserdataService) {
+
+        UserdataService.getFirstUsername().then(function(firstUsername) {
+            $scope.firstUsername = firstUsername;
+        }); 
+
+    });
+
+controllers.controller('DashController', function ($scope, UserdataService) {
+
+        UserdataService.getFirstUsername().then(function(firstUsername) {
+            $scope.firstUsername = firstUsername;
+        }); 
+
+    });
+controllers.controller('VoucherController', function ($scope, UserdataService) {
 
         UserdataService.getFirstUsername().then(function(firstUsername) {
             $scope.firstUsername = firstUsername;
