1e3a0c91638aa2b9797f277944e4bd97858fd7dd
e04mk <e04mk@esek.lth.se>
2015-02-07 22:53:25 +0100

    Updates to dash and home

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -20,10 +20,10 @@ controllers.controller('DashController', function ($scope, UserdataService) {
         }); 
 
     });
-controllers.controller('VoucherController', function ($scope, UserdataService) {
+controllers.controller('VoucherController', function ($scope, $http) {
 
-        UserdataService.getFirstUsername().then(function(firstUsername) {
-            $scope.firstUsername = firstUsername;
-        }); 
-
+        $http.get('http://localhost:3000/api/v1/org/ACME/years/2015/vouchers').success(function(data, status, headers, config) {
+            $scope.vouchers = data;
+            $scope.vouchersStatus = status;
+        });
     });
