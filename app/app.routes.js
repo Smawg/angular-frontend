293c1c660a2914070746680173da371e78f4fd64
Niklas Claesson <nicke.claesson@gmail.com>
2015-02-07 22:47:04 +0100

    wip

--- app/app.routes.js
+++ app/app.routes.js
@@ -7,12 +7,17 @@ SmawgApp.config(function($stateProvider, $urlRouterProvider) {
         templateUrl: 'app/components/dash/dashView.html',
         controller: 'DashController'
       })
-      .state('/voucher/:voucherId', {
+      .state('home', {
+        url: '/home',
+        templateUrl: 'app/components/home/homeView.html',
+        controller: 'DashController'
+      })
+      .state('about', {
+        templateUrl: 'app/components/about/aboutView.html',
+        controller: 'HomeController'
+      })
+      .state('voucher/:voucherId', {
         templateUrl: 'app/voucher/voucherView.html',
         controller: 'VoucherController'
-      })
-      .state('/home', {
-        templateUrl: 'app/components/home/homeView.html',
-        controller: 'HomeController'
       });
   });
