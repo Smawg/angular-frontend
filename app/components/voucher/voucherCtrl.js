70f24157ef6648fa772d699a89c6023d78bc5569
e04mk <e04mk@esek.lth.se>
2015-02-15 15:50:28 +0100

    Basic functionality for "add voucher to DB"

--- app/components/voucher/voucherCtrl.js
+++ app/components/voucher/voucherCtrl.js
@@ -3,8 +3,40 @@ angular.module('SmawgApp.controllers').controller('VoucherCtrl', function ($scop
     $scope.vouchers = data;
     $scope.vouchersStatus = status;
   });
-  $scope.AccountRows=[{account_number: "", debet: "", kredit:""}];
+  $scope.voucherNbr = 0;
+  $scope.description = "";
+  $scope.date = "";
+  $scope.AccountRows=[{account_number: 0, debit: 0, credit: 0}];
   $scope.addAccountRow = function(){
-    $scope.AccountRows.push({account_number: "", debet: "", kredit: ""});
+    $scope.AccountRows.push({account_number: 0, debit: 0, credit: 0});
   };
+  
+  $scope.removeAccountRow = function(index) {
+    $scope.AccountRows.splice(index, 1);
+  };
+
+  $scope.TotalDebit = function(){
+    var sum = 0;
+    angular.forEach($scope.AccountRows, function(item) {
+      sum += item.debit;
+    });
+    return sum;
+  };
+
+ $scope.TotalCredit = function(){
+    var sum = 0;
+    angular.forEach($scope.AccountRows, function(item) {
+      sum += item.credit;
+    });
+    return sum;
+  };
+
+  $scope.SaveVoucher = function() {
+    var command = ['http://localhost:3000/api/v1/org/ACME/years/2015/vouchers', {"voucher": {"number": $scope.voucherNbr, "description": $scope.description, "date": $scope.date, "voucher_rows_attributes": $scope.AccountRows}}];
+    console.log(command);
+    $http.post('http://localhost:3000/api/v1/org/ACME/years/2015/vouchers', {"voucher": {"number": $scope.voucherNbr, "description": $scope.description, "date": $scope.date, "voucher_rows_attributes": $scope.AccountRows}}).success(function(data, status, headers, config) {
+      console.log("successfully added voucher to DB");
+    });
+  };
+
 });
