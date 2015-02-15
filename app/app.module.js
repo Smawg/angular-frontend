ae9b775eada246722f14ec34b3977109675a98b3
Niklas Claesson <nicke.claesson@gmail.com>
2015-02-15 14:23:25 +0100

    Fixing menu

--- app/app.module.js
+++ app/app.module.js
@@ -1,13 +1,25 @@
+/*
+ * Declaration of the "main" module. Declares dependencies.
+ */
 var SmawgApp = angular.module('SmawgApp',
     [
         'ui.router',
         'ngMaterial',
         'restangular',
         'SmawgApp.controllers',
+        'SmawgApp.directives',
         'SmawgApp.services'
     ]
 );
+
+/*
+ * Declaration of module containing the controllers.
+ */
 angular.module('SmawgApp.controllers', []);
+/*
+ * Declaration of module containing the directives..
+ */
+angular.module('SmawgApp.directives', []);
 
 
 SmawgApp.config(function($mdThemingProvider) {
--- app/shared/nav/navCtrl.js
+++ app/shared/nav/navCtrl.js
@@ -1,7 +1,28 @@
 angular.module('SmawgApp.controllers').controller('SmawgNavCtrl', ['$scope', '$mdSidenav', function ($scope, $mdSidenav) {
-  $scope.toggleSideNav = function (menuId) {
-    $mdSidenav(menuId).toggle();
+  $scope.openMenu = function () {
+    $mdSidenav("left").open();
   };
+  $scope.closeMenu = function() {
+    $mdSidenav("left").close();
+  };
+  $scope.sections = [
+    {
+      name:"Home",
+      sref:"home",
+      type:"link"
+    }, {
+      name:"Dashboard",
+      sref:"dash",
+      type:"link"
+    }, { 
+      name:"Voucher",
+      sref:"voucher.overview",
+      type:"link"
+    }, {
+      name:"About",
+      sref:"about",
+      type:"link"
+    }];
 }]);
 
 
