d2f733d8cecb587df794b25c74ef76d4f5a7020e
e04mk <e04mk@esek.lth.se>
2015-02-23 17:31:44 +0100

    Added som more to editing of vouchers. Added messages-module to dependencies.

--- app/components/voucher/voucherCtrl.js
+++ app/components/voucher/voucherCtrl.js
@@ -1,70 +1,92 @@
-angular.module('SmawgApp.services')
-  .factory('VoucherService', ['Restangular', '$q', 'AuthService', function VoucherService(Restangular, $q, AuthService) {
-    var org = AuthService.getUserInfo().organisation;
-    Restangular.setBaseUrl('http://localhost:3000/api/v1/org/' + org + '/years/2015');
-    var vouchers = Restangular.all('vouchers');
-    return {
-      getVouchers : function() {
-        var vouchersDeferred = $q.defer();
-        var response = vouchers.getList().then(function(response) {
+angular.module('SmawgApp.services').factory('VoucherService', ['Restangular', '$q', 'AuthService', function VoucherService(Restangular, $q, AuthService) {
+  "use strict";
+  var org = AuthService.getUserInfo().organisation,
+    vouchers = Restangular.all('vouchers'),
+    voucher = Restangular.all('vouchers');
+  Restangular.setBaseUrl('http://localhost:3000/api/v1/org/' + org + '/years/2015');
+  return {
+    getVouchers : function () {
+      var vouchersDeferred = $q.defer(),
+        response = vouchers.getList().then(function (response) {
           vouchersDeferred.resolve(response);
         });
-        return vouchersDeferred.promise;
-      },
-      saveVoucher : function(voucher) {
-        var vouchersDeferred = $q.defer();
-        var response = vouchers.post(voucher).then(function(response) {
+      return vouchersDeferred.promise;
+    },
+    saveVoucher : function (voucher) {
+      var vouchersDeferred = $q.defer(),
+        response = vouchers.post(voucher).then(function (response) {
           vouchersDeferred.resolve(response);
         });
-        return vouchersDeferred.promise;
-      }
-    };
-  }]);
-angular.module('SmawgApp.controllers').controller('VoucherCtrl', function ($scope, VoucherService) {
-  VoucherService.getVouchers().then(function(data){
+      return vouchersDeferred.promise;
+    },
+    updateVoucher : function (voucher) {
+      var vouchersDeferred = $q.defer(),
+        response = Restangular.all('vouchers/:id').patch("vouchers",voucher).then(function (response) {
+          vouchersDeferred.resolve(response);
+        });
+      return vouchersDeferred.promise;
+    },
+    getFullVoucher : function (voucherNbr) {
+      var voucherDeferred = $q.defer(),
+        response = voucher.get(":" + voucherNbr).then(function (response) {
+          voucherDeferred.resolve(response);
+        });
+      return voucherDeferred.promise;
+    }
+  };
+}]);
+angular.module('SmawgApp.controllers',['ngMessages']).controller('VoucherCtrl', function ($scope, VoucherService) {
+  "use strict";
+  VoucherService.getVouchers().then(function (data) {
     console.log(data);
     $scope.vouchers = data;
   });
-  $scope.AccountRows=[];
-  $scope.editVoucher=false;
-  $scope.addAccountRow = function(){
+  $scope.getFullVoucher = function (voucherNbr) {
+    VoucherService.getFullVoucher(voucherNbr).then(function (data) {
+      console.log(data);
+      return data;
+    });
+  };
+  $scope.AccountRows = [];
+  $scope.editVoucher = false;
+  $scope.addAccountRow = function () {
     $scope.AccountRows.push({account_number: 0, debit: 0, credit: 0});
   };
-  
-  $scope.removeAccountRow = function(index) {
+
+  $scope.removeAccountRow = function (index) {
     $scope.AccountRows.splice(index, 1);
   };
 
-  $scope.TotalDebit = function(){
+  $scope.TotalDebit = function () {
     var sum = 0;
-    angular.forEach($scope.AccountRows, function(item) {
+    angular.forEach($scope.AccountRows, function (item) {
       sum += item.debit;
     });
     return sum;
   };
 
- $scope.TotalCredit = function(){
+  $scope.TotalCredit = function () {
     var sum = 0;
-    angular.forEach($scope.AccountRows, function(item) {
+    angular.forEach($scope.AccountRows, function (item) {
       sum += item.credit;
     });
     return sum;
   };
 
 
-  $scope.toggleEdit = function() {
-    $scope.editVoucher = !($scope.editVoucher);  
+  $scope.toggleEdit = function () {
+    $scope.editVoucher = !($scope.editVoucher);
   };
-    
-  $scope.SaveVoucher = function() {
+
+  $scope.SaveVoucher = function () {
     var voucher = {"voucher": {"number": $scope.voucherNbr, "description": $scope.description, "date": $scope.date, "voucher_rows_attributes": $scope.AccountRows}};
     VoucherService.saveVoucher(voucher);
-    
+
     console.log("successfully added voucher to DB");
   };
-  
-  $scope.SaveEditedVoucher= function($http,index, voucherNbr, description, date, voucher_rows_attributes){
-    var voucher = {"voucher": {"number": voucherNbr, "description": description, "date": date, "voucher_rows_attributes": voucher_rows_attributes}};
-    $http.post("http://localhost:3000/api/v1/org/ACME/years/2015/vouchers/:" + index, voucher);
+
+  $scope.updateVoucher = function ($http, index, voucherNbr, description, date, voucher_rows) {
+    var voucher = {"voucher": {"number": voucherNbr, "description": description, "date": date, "voucher_rows_attributes": voucher_rows}};
+    VoucherService.updateVoucher(voucher);
   };
 });
